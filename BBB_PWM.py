e30582a99fa3eecffad15afba42d22b13059b0d7
root <root@beaglebone.localdomain>
2019-11-01 17:55:30 +0000

    general test for PWM on the Beaglebone

--- BBB_PWM.py
+++ BBB_PWM.py
@@ -1,4 +1,17 @@
-from Adafruit_BBIO.PWM import PWM
+import Adafruit_BBIO.PWM as PWM
 import time
 
+pwm_pin = "P9_16"
 
+
+PWM.start(pwm_pin, 0, 1000)# EHRPWM1B
+
+while True:
+    voltage = input("Enter Voltage Level: ")
+    DC_sim = voltage/3.365*100
+    if DC_sim > 100:
+        DC_sim = 100
+    PWM.set_duty_cycle(pwm_pin, DC_sim)
+
+PWM.stop(pwm_pin)
+PWM.cleanup()
