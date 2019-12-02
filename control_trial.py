d8c97a5d31af95801259c60ad40fa14b7487b884
Bruno <brunorodriguez1998@gmail.com>
2019-12-02 14:09:12 -0800

    12-02-19

--- control_trial.py
+++ control_trial.py
@@ -2,7 +2,7 @@ import time
 import math
 import Adafruit_BBIO.ADC as ADC
 import Adafruit_BBIO.PWM as PWM
-from  Adafruit_BBIO.SPI import SPI
+from Adafruit_BBIO.SPI import SPI
 import Adafruit_BBIO.GPIO as GPIO
 
 cs1 = "P9_23"
@@ -17,11 +17,14 @@ TX_pin = "P9_41"
 segA = "P8_7"
 segB = "P8_8"
 
-joystickCenterList = [0,0,0,0,0,0,0,0,0,0]
+joystickCenter1 = [0,0,0,0,0,0,0,0,0,0]
+joystickCenter2 = [0,0,0,0,0,0,0,0,0,0]
 joystickForwardList = [0,0,0,0,0,0,0,0,0,0]
 joystickBackwardsList = [0,0,0,0,0,0,0,0,0,0]
 joystickLeftList = [0,0,0,0,0,0,0,0,0,0]
 joystickRightList = [0,0,0,0,0,0,0,0,0,0]
+joystickDirectionList = [0,0,0,0,0,0,0,0,0,0]
+joystickSpeedList = [0,0,0,0,0,0,0,0,0,0]
 
 clbCenter = [0,0]
 
@@ -53,40 +56,60 @@ def map_value(x, in_min, in_max, out_min, out_max):
 def rounder(x, base=5):
      return int(base * round(float(x)/base))
 
-def calib(pin, index):
-    length = len(lister)
+def calib(index):
+    length = 10
     calibration_flag = True
     recalibration = True
     # for calibrating the center
     if index == 0:
         while calibration_flag:
             recalibration = False
-            print("Calibrating Center Joystick...")
-            time.sleep(3)
+            print("Calibrating Joystick Center...")
+            input("Press Enter to Continue: ")
+            time.sleep(0.5)
             for i in range(length):
-                if abs(joystickSpeed[i]-joystickSpeed[i-1]) > 10 or abs(joystic:
+                if abs(joystickCenter1[i]-joystickCenter1[i-1]) > 10 or abs(joystickCenter2[i]-joystickCenter2[i-1]) > 10:
                     print("Error in Calibration. Recalibrating...")
                     recalibration = True
                     time.sleep(1)
                     break
-                joystickCenter.append(ADC.read_raw(pin))
-                print(joystickCenter[i])
+                joystickCenter1.append(ADC.read_raw(ADC_pin1))
+                joystickCenter2.append(ADC.read_raw(ADC_pin2))
+                print(joystickCenter1[i])
+                print(joystickCenter2[i])
                 time.sleep(0.5)
 
             if recalibration:
                 continue
             else:
                 calibration_flag = False
+                break
         
-        return [sum(joystickCenter)/len(joystickCenter),sum(joystickDirection)/len(joystickDirection)]
-
+        return [sum(joystickCenter1)/len(joystickCenter1),sum(joystickCenter2)/len(joystickCenter2)]
+    # for calibrating the joystick Direction
     if index == 1:
-        
-
+        while calibration_flag:
+            recalibration = False
+            print("Calibrating Joystick Direction...")
+            input("Press Enter to Continue: ")
+            time.sleep(0.5)
+            for i in range(length):
+                if abs(joystickDirectionList[i]-joystickDirectionList[i-1]) > 10:
+                    print("Error in Calibration. Recalibrating...")
+                    recalibration = True
+                    time.sleep(1)
+                    break
+                joystickDirectionList.append(ADC.read_raw(ADC_pin1))
+                print(joystickDirectionList[i]
+                time.sleep(0.5)
             
-
-
+            if recalibration:
+                continue
+            else:
+                calibration_flag = False
+                break
                 
+        return sum(joystickDirection)/len(joystickDirection)
 
 
 GPIO.setup(cs1, GPIO.OUT)
@@ -109,7 +132,7 @@ spi2.cshigh = True
 
 
 while True:
-    clbCenter = calib(ADC_pin1, 0)
+    clbCenter = calib(0)
     joystickSpeed = ADC.read_raw(ADC_pin1)
     joystickDirection = ADC.read_raw(ADC_pin2)
     
@@ -123,8 +146,6 @@ while True:
     
     joystickSpeed = rounder(int(joystickSpeed))
     joystickDirection = rounder(int(joystickDirection))
-    
-    
 
     print(joystickSpeed)
     print(joystickDirection)
